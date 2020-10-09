3b64c1dbbbabc4a0be92668f6392c911aa887ea3
paradoxwastaken <63323642+paradoxwastaken@users.noreply.github.com>
2020-10-09 00:03:51 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,10 @@
 #include "hwid.h"
 
 int main() {
-	auto HWID{ HardwareId() };
+    auto HWID{ HardwareId() };
 
-	std::cout << HWID.Disk.SerialNumber << std::endl;
-	std::cout << HWID.SMBIOS.Manufacturer << std::endl;
-	std::cout << HWID.CPU.Architecture << std::endl;
-	std::cout << HWID.Windows.ComputerHardwareId << std::endl;
+    printf(HWID.Disk.SerialNumber);
 
-	for (int i = 0; i < HWID.CPU.Features.size(); i++) {
-		std::cout << HWID.CPU.Features.at(i) << std::endl;
-	}
-
-	getchar();
+    getchar();
     return 0;
 }
